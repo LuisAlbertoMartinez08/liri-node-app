949c795014444501a889e1539f57a95f097f7e9a
Luis Martinez <One2@Luiss-MacBook-Pro.local>
2016-12-14 18:36:51 -0500

    added spotify javascript

--- liri.js
+++ liri.js
@@ -1,4 +1,15 @@
-var Twitter = require('twitter');var twitterKey = require('./keys.js');
+
+var spotify = require('spotify')
+var input1 = process.argv[2];
+var input2 = process.argv[3];
+
+
+
+//--------tweeter
+
+if (input1 == 'my-tweets') {
+var Twitter = require('twitter');
+var twitterKey = require('./keys.js');
 var client = new Twitter({
   consumer_key: twitterKey.twitterKeys.consumer_key,
   consumer_secret: twitterKey.twitterKeys.consumer_secret,
@@ -16,3 +27,22 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 
 });
+};
+
+
+//----------spotify
+
+if(input1 == 'my-song'){
+    spotifyThis(input2);
+}
+
+function spotifyThis(input2){
+spotify.search({ type: 'track', query: input2 }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+   console.log(data.tracks.items[0].name);
+});
+}
